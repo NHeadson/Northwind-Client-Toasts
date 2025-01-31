97a309b65e56b467f028da664a5c004e06300139
NHeadson <nheadson@my.wctc.edu>
2025-01-30 18:47:47 -0600

    Initial

---
+++ discount.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("discount-row")
+    .addEventListener("click", function (e) {
+      if (e.target.classList.contains("discount")) {
+        e.preventDefault();
+        document.getElementById("product").innerHTML =
+          e.target.dataset["product"];
+        bootstrap.Toast.getOrCreateInstance(
+          document.getElementById("liveToast")
+        ).show();
+      }
+    });
+});
